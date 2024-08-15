005342a43c187cd7cae1ad0659b1ec560763268a
DiegoWill <diego.wcarvalho@minervafoods.com>
2024-08-15 15:54:03 -0300

    creating new function on unit test to avoid repetion and changing emotion_detector return

--- EmotionDetection/tests/test_emotion_detection.py
+++ EmotionDetection/tests/test_emotion_detection.py
@@ -3,20 +3,26 @@ import unittest
 from EmotionDetection.services.emotion_detection import emotion_detector
 
 class TestEmotionDetection(unittest.TestCase):
+
+    def check_emotion(self, text, expected_emotion):
+        response = emotion_detector(text)
+        dominant_emotion = response['dominant_emotion']
+        self.assertEqual(dominant_emotion, expected_emotion)
+
     def test_joy(self):
-        self.assertEqual(emotion_detector("I am glad this happened"), 'joy')
+        self.check_emotion("I am glad this happened", 'joy')
 
     def test_anger(self):
-        self.assertEqual(emotion_detector("I am really mad about this"), 'anger')
+        self.check_emotion("I am really mad about this", 'anger')
 
     def test_disgust(self):
-        self.assertEqual(emotion_detector("I feel disgusted just hearing about this"), 'disgust')
+        self.check_emotion("I feel disgusted just hearing about this", 'disgust')
 
     def test_sadness(self):
-        self.assertEqual(emotion_detector("I am so sad about this"), 'sadness')
+        self.check_emotion("I am so sad about this", 'sadness')
 
     def test_fear(self):
-        self.assertEqual(emotion_detector("I am really afraid that this will happen"), 'fear')
+        self.check_emotion("I am really afraid that this will happen", 'fear')
 
 if __name__ == '__main__':
     unittest.main()
