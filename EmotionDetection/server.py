8f23bbb8ab09a40d684768cb389a1bfcff0d0aa6
DiegoWill <diego.wcarvalho@minervafoods.com>
2024-08-15 17:48:35 -0300

    adding docstrings for server.py

--- EmotionDetection/server.py
+++ EmotionDetection/server.py
@@ -1,3 +1,12 @@
+"""
+server.py
+
+This module sets up a Flask web application with two routes:
+1. '/' - Renders the index.html template.
+2. '/emotionDetector/<path:text>' - Analyzes the text and
+   searches for words related to emotions.
+"""
+
 from flask import Flask, render_template
 from EmotionDetection.services.emotion_detection import emotion_detector
 
@@ -5,10 +14,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_index_html():
+    """Render the main page"""
     return render_template('index.html')
 
 @app.route('/emotionDetector/<path:text>')
 def analyze_text(text):
+    """Receives the text parameter and analyzes it to identify the emotion"""
     analysis_result = emotion_detector(text)
     return analysis_result
 
